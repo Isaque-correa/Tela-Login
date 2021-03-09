861c0695a746f90e289816414016c02fcb0eee41
Isaque correa <isq2006@gmail.com>
2021-03-09 20:31:34 -0300

    Primeiro commit.

---
+++ js/validacao.js
@@ -0,0 +1,29 @@
+
+var formulario = $('form');
+formulario.submit(function(event){
+
+    event.preventDefault();
+   var self = $(this);
+   var texBox = self.find('input[type="text"],input[type="email"],input[type="password"]');
+   $('.mensagem_erro').text('');
+    
+    
+   texBox.each(function(){
+        var item = $(this);
+        var value = item.val();
+        var obrigatorio = item.prop('required');
+        var mensagemErro = item.data('msg-obrigatorio');
+        var formGrup = item.parent();
+        var mensagemErroSpan = formGrup.find('.mensagem_erro');
+        var expressaoErro = item.rag
+        item.removeClass('borda-vermelha');
+        
+        if(value.length == 0 && obrigatorio == true){
+            item.addClass('borda-vermelha');
+            mensagemErroSpan.text(mensagemErro);
+            
+        }
+   });
+});
+
+

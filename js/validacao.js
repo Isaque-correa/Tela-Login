8f3b8c5a8e855d213b3587f47d354033d123cd17
Isaque correa <isq2006@gmail.com>
2021-03-30 16:27:34 -0300

    implementação da confirmção de senha do cadastro

--- js/validacao.js
+++ js/validacao.js
@@ -6,11 +6,13 @@ formulario.submit(function(event){
    var self = $(this);
    var texBox = self.find('input[type="text"],input[type="email"],input[type="password"]');
    $('.mensagem_erro').text('');
-    
+    var senhaAtual = '';
     
    texBox.each(function(){
         var item = $(this);
         var value = item.val();
+        var name = item[0].name;
+        console.log(name)
         var obrigatorio = item.prop('required');
         var mensagemErro = item.data('msg-obrigatorio');
         var formGrup = item.parent();
@@ -20,8 +22,15 @@ formulario.submit(function(event){
         if(value.length == 0 && obrigatorio == true){
             item.addClass('borda-vermelha');
             mensagemErroSpan.text(mensagemErro);
+        }  
+        //validação da confirmação de senha
+        if(name == 'senha'){
+            senhaAtual = value;
+        }
+        
+        if(name == 'confirmaSenha' && value != senhaAtual && value.length > 0 ){
+            item.addClass('borda-vermelha');
+            mensagemErroSpan.text(item.attr('data-msg-confirmaSenha'));
         }
    });
 });
-
-
